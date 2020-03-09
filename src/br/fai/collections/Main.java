c63f778feda6cc9f085b5555bc74187f7822f0fb
Matuk <mateusmatuk@fai-mg.br>
2020-03-09 20:57:28 -0300

    acabou essa budega

--- src/br/fai/collections/Main.java
+++ src/br/fai/collections/Main.java
@@ -16,20 +16,47 @@ public class Main {
 
 	}
 	private Scanner scan = new Scanner(System.in);
+	private final String VALOR_INVALIDO = "VALOR_INVALIDO"; 
 	
 	private void Start() {
-		
 		List<String> lista = new ArrayList<String>();
-		String valor = obterDados();
-		System.out.println(valor);
 		
+		while(lista.size() < 5)
+		{
+			String valor = obterDados();
+			
+			if(valor == VALOR_INVALIDO || valor.isEmpty())
+			{
+				System.out.println("Valor nao foi adicionado.");
+			}
+			else {
+				System.out.println(valor);
+				lista.add(valor);
+			}
+		}
+	
+		lista.add("item adicionado automaticamente 01");
+		lista.add("item adicionado automaticamente 02");
+		lista.remove(1);
+		lista.remove(1);
+		
+		for(String item : lista) {
+			System.out.println("O item da lista eh: " + item);
+		}
 	}
 	
 	private String obterDados() {
-		
-		int valor = scan.nextInt();
-		return String.valueOf(valor);		
-		
+
+		try {
+			int valor = scan.nextInt();
+			return String.valueOf(valor);		
+			
+		} catch (Exception e) {
+			scan.next();
+			System.out.println("Valor invalido digitado.");
+			return VALOR_INVALIDO;
+		}
+
 	}
 
 }
