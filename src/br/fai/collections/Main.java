8faeea634834bc859b03a9ea8b02491592b0ed69
Matuk <mateusmatuk@fai-mg.br>
2020-03-09 20:32:06 -0300

    segunda rodada

--- src/br/fai/collections/Main.java
+++ src/br/fai/collections/Main.java
@@ -1,10 +1,35 @@
 package br.fai.collections;
 
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		Main app = new Main();
+		app.Start();
+		
 
+	}
+	private Scanner scan = new Scanner(System.in);
+	
+	private void Start() {
+		
+		List<String> lista = new ArrayList<String>();
+		String valor = obterDados();
+		System.out.println(valor);
+		
+	}
+	
+	private String obterDados() {
+		
+		int valor = scan.nextInt();
+		return String.valueOf(valor);		
+		
 	}
 
 }
